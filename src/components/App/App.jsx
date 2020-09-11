055e975d6a489ac497c50d5bbb6a4228d7c7da0e
Samuel Braga Gomes Fogaça <samuelbgf@hotmail.com>
2020-09-11 11:11:35 -0300

    add LineChart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Checkbox from '../../shared/Checkbox'
+import LineChart from '../../shared/LineChart/LineChart'
 import AppContainer from '../AppContainer'
 import AppHeader from '../AppHeader'
 import { Container, Wrapper } from './App.styles'
@@ -7,6 +8,8 @@ import { Container, Wrapper } from './App.styles'
 function App() {
   const [lettuce, setLetucce] = useState(false)
   const [rice, setRice] = useState(false)
+
+  const colors= ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
 
     return <Wrapper>
       <Container>
@@ -27,10 +30,21 @@ function App() {
            />
          </div>}
          middle={<div>
-           sua lista de compras
+           sua lista de compras:
+
+           <Checkbox
+             value={rice}
+             title="Arroz"
+             onClick={() => setRice(!rice)}
+           />
          </div>}
          right={<div>
            estatisticas
+
+           <LineChart color={colors[0]} title="saudavel" percentage={80}/>
+           <LineChart color={colors[1]} title="não tão saudavel" percentage={20}/>
+           <LineChart color={colors[2]} title="limpeza" percentage={35}/>
+           <LineChart color={colors[3]} title="outros" percentage={15}/>
          </div>}
         />
       </Container>
