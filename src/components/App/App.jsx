d8a5ab3064de00af993555b29ab0e7a7e9581112
Samuel Braga Gomes Fogaça <samuelbgf@hotmail.com>
2020-09-11 09:21:37 -0300

    add base layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import AppContainer from '../AppContainer'
+import AppHeader from '../AppHeader'
 import { Container, Wrapper } from './App.styles'
 
 function App() {
     return <Wrapper>
       <Container>
-        todo: app
+        <AppHeader />
+        <AppContainer
+         left={<div style={{ backgroundColor: 'red'}}>
+           produtos disponoveis
+         </div>}
+         middle={<div style={{ backgroundColor: 'green'}}>
+           sua lista de compras
+         </div>}
+         right={<div style={{ backgroundColor: 'blue'}}>
+           estatisticas
+         </div>}
+        />
       </Container>
     </Wrapper>
 }
