f8c146cd32a19859b2bb14539876b6dcf6445953
Samuel Braga Gomes Fogaça <samuelbgf@hotmail.com>
2020-09-14 18:16:22 -0300

    make shopping list smart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,13 +35,12 @@ function App() {
         left={
           <ShoppingList
             title="Produtos disponíveis"
-            products={products}
             onToggle={handleToggle}
           />}
         middle={
           <ShoppingList
             title="Sua lista de compras"
-            products={selectedProducts}
+            displayOnlySelected
             onToggle={handleToggle}
           />}
         right={
